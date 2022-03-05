b1f08e4d222b523a3fec74b56be64853876301c8
Hubert Strauss <82213918+HubertStr@users.noreply.github.com>
2022-03-04 20:28:35 -0500

    Initialize kernel + iterations kernel

--- kernel.cu
+++ kernel.cu
@@ -1,7 +1,9 @@
 //Kernel to Scale velocity and position values to be between the max and min (and not between 0 and 1 anymore)
 // AND initialize particle best (for each particle) + local best 
 
-__global__void Scale_Init(int *xmax, int *xmin, float *pos, float *velocity, float *p_best_y, int *l_best_index, int *best_index, curandState *states)
+#define inf 9999.99f
+
+__global__void Scale_Init(float *xmax, float *xmin, float *pos, float *velocity, float *p_best_y, int *l_best_index, int *best_index, curandState *states)
 {
     int index = blockDim.x * blockIdx.x + threadIdx.x;
     int t_index = threadIdx.x;
@@ -25,7 +27,70 @@ __global__void Scale_Init(int *xmax, int *xmin, float *pos, float *velocity, flo
     //Each thread gets a different seed, different sequence number and no offset
     curand_init(index, index, 0, &states[index]);
 }
-// 1 kernel to compute the actual iterations of the updates 
 
-// 1 Kernel to compute the global best after the iterations
+// Kernel to compute the actual iterations of the updates 
+_global_void Iterate(float *xmax, float *xmin, float *pos, float *velocity, float *p_best_pos,float *p_best_y, int *l_best_index, int *best_index, curandState *states, float *c_1, float *c_2)
+{
+    int index = threadIdx.x + blockDim.x * blockIdx.x;
+    
+    float r1, r2;
+    
+    float personal_best;
+    int local_best;
+    curandState local_state = states[index];
 
+  
+    //Calculate fitness of particle
+    float fitness = 0.0f;
+    for (int i = 0; i < D; i++)
+        fitness += pos[index * D + i] * pos[index * D +i];
+  
+   
+    //Set PBest if fitness is better
+    if (p_best_y[index] > fitness)
+    {
+      p_best_y[index] = fitness;
+      for (int i = 0; i < D; i++)
+        p_best_pos[index * D + i] = pos[index * D + i];
+    }
+    personal_best = p_best_y[index];
+    
+    //Set left and right neighbours
+    int left = (N + index - 1) % N;
+    int right = (1 + index) % N;
+    
+    //Set the local best index
+    if (p_best_y[left] < personal_best)
+      l_best_index[index] = left;
+    if (p_best_y[right] < personal_best)
+      l_best_index[index] = right;
+    local_best = l_best_index[index];
+    
+    //Update the particle velocity and position
+    for (int i = 0; i < D; i++)
+    {
+      int j = index * D + i;
+      r1 = curand_uniform(&local_state);
+      r2 = curand_uniform(&local_state);
+      
+      // Compute the velocity
+      velocity[j] = chi * (velocity[j] + (c1 * r1 * (p_best_pos[j] - pos[j])) + (c2 * r2 * (p_best_pos[local_best] - pos[j])));
+      
+      //Ensure velocity values are within range
+      if (velocity[j] > (xmax - xmin) )
+        velocity[j] = (xmax - xmin);
+      if (velocity[j] < -(xmax - xmin))
+        velocity[j] = -(xmax - xmin);
+      
+      //Update the position ensuring all values are within the xmin to xmax range
+      pos[j] = pos[j] + velocity[j];
+      if (pos[j] > xmax)
+        pos[j] = xmax;
+      if (pos[j] < xmin)
+        pos[j] = xmin;
+    }
+    
+    //Set the current state of the PRNG
+    states[index] = local_state;
+      
+}
